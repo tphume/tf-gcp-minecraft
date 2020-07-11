f5f4243961258128bfab959dabc69f368e5ce261
Traphume <traphumedev@gmail.com>
2020-07-12 00:02:39 +0700

    Added static external ip address to ge instace

--- ce/main.tf
+++ ce/main.tf
@@ -8,6 +8,11 @@ data "google_compute_image" "cos" {
   family = "cos-stable"
 }
 
+resource "google_compute_address" "minecraft" {
+  name         = var.ce
+  address_type = "EXTERNAL"
+}
+
 resource "google_compute_instance" "minecraft" {
   name         = var.ce
   machine_type = "f1-micro"
@@ -16,6 +21,13 @@ resource "google_compute_instance" "minecraft" {
     initialize_params {
       type  = "pd-standard"
       image = data.google_compute_image.cos
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      nat_ip = google_compute_address.minecraft.address
     }
   }
 }
