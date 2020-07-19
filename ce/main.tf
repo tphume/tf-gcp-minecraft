28d8d474b2f055b98db1d384beb6573913451c5e
Traphume <traphumedev@gmail.com>
2020-07-20 02:36:42 +0700

    Fixed command

--- ce/main.tf
+++ ce/main.tf
@@ -64,7 +64,7 @@ resource "google_compute_instance" "minecraft" {
 
   metadata_startup_script = <<EOT
   docker run -d -p 25565:25565 -e VERSION=${var.mc_version} -e MEMORY=6G \
-  -e EULA=TRUE -v "$(pwd)"mc:/data \
+  -e EULA=TRUE -v "$(pwd)"/mc:/data \
   --restart always --name mc itzg/minecraft-server
   EOT
 
