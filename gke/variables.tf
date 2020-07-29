7df66ff030ecef762b1f8955bfac97cf8133d3af
Traphume <traphumedev@gmail.com>
2020-07-29 20:19:18 +0700

    Added variables need for provider

---
+++ gke/variables.tf
@@ -0,0 +1,14 @@
+variable "project" {
+  description = "Your GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "Region to create resource in"
+  type        = string
+}
+
+variable "zone" {
+  description = "Zone to create resource in"
+  type        = string
+}
